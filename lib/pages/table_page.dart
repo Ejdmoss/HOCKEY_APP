21985d4b1a6985cfa4f3a4041c0cf36f6b5e6efb
Ejdmoss <adaamstuchlik@gmail.com>
2024-10-03 17:52:29 +0200

    37-version

--- lib/pages/table_page.dart
+++ lib/pages/table_page.dart
@@ -26,7 +26,6 @@ class _TablePageState extends State<TablePage> {
       ),
       body: Column(
         children: [
-          
           // reading realtime database
           Expanded(
             child: FirebaseAnimatedList(
@@ -38,105 +37,19 @@ class _TablePageState extends State<TablePage> {
                   ),
                   margin: EdgeInsets.all(10),
                   child: ListTile(
-                    contentPadding: EdgeInsets.symmetric(
+                    contentPadding: const EdgeInsets.symmetric(
                       horizontal: 10,
                       vertical: 5,
                     ),
-                    leading: CircleAvatar(
-                      child: Text(
-                        (index + 1).toString(),
-                      ),
-                    ),
-                    title: Text(snapshot.child("name").value.toString(),
+                    title: Text(
+                      snapshot.child("name").value.toString(),
                     ),
                   ),
                 );
               },
             ),
           ),
-         
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          placementController.clear();
-          nameController.clear();
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return myDialogBo(
-                  context: context,
-                  name: "Create",
-                  placement: "add",
-                  onPressed: () {
-                    final id = DateTime.now().millisecondsSinceEpoch.toString();
-                    databaseReference.child(id).set({
-                      'name': nameController.text.toString(),
-                      "address": placementController.text.toString(),
-                      'id': id
-                    });
-                    Navigator.pop(context);
-                  },
-                );
-              });
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-
-  // dialog box
-  Dialog myDialogBo({
-    required BuildContext context,
-    required String name,
-    required String placement,
-    required VoidCallback onPressed,
-  }) {
-    return Dialog(
-      backgroundColor: Colors.yellow,
-      child: Container(
-        decoration: const BoxDecoration(color: Colors.red),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  name,
-                  style: const TextStyle(color: Colors.green),
-                ),
-                CircleAvatar(
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(Icons.close),
-                  ),
-                ),
-              ],
-            ),
-            TextField(
-              controller: nameController,
-              decoration: const InputDecoration(labelText: "Enter the name"),
-            ),
-            TextField(
-              controller: placementController,
-              decoration:
-                  const InputDecoration(labelText: "Enter the placement"),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: onPressed,
-              child: Text(placement),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-          ],
-        ),
       ),
     );
   }
