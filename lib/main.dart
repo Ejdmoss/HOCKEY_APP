da77b25cac272195410cef2cb59ed1f329130c8b
Ejdmoss <adaamstuchlik@gmail.com>
2024-10-03 17:48:55 +0200

    36-version

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hockey_app/sevices/auth/auth_gate.dart';
-import 'package:hockey_app/firebase_options.dart';
 import 'package:hockey_app/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
   
   runApp(
     ChangeNotifierProvider(
--- lib/pages/table_page.dart
+++ lib/pages/table_page.dart
@@ -1,8 +1,19 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
-class TablePage extends StatelessWidget {
+class TablePage extends StatefulWidget {
   const TablePage({super.key});
 
+  @override
+  State<TablePage> createState() => _TablePageState();
+}
+
+final databaseReference = FirebaseDatabase.instance.ref('teams');
+
+class _TablePageState extends State<TablePage> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController placementController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +23,121 @@ class TablePage extends StatelessWidget {
           padding: EdgeInsets.only(left: 95),
           child: Text("Table"),
         ),
-    ),
+      ),
+      body: Column(
+        children: [
+          
+          // reading realtime database
+          Expanded(
+            child: FirebaseAnimatedList(
+              query: databaseReference,
+              itemBuilder: (context, snapshot, animation, index) {
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 5,
+                    ),
+                    leading: CircleAvatar(
+                      child: Text(
+                        (index + 1).toString(),
+                      ),
+                    ),
+                    title: Text(snapshot.child("name").value.toString(),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+         
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          placementController.clear();
+          nameController.clear();
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return myDialogBo(
+                  context: context,
+                  name: "Create",
+                  placement: "add",
+                  onPressed: () {
+                    final id = DateTime.now().millisecondsSinceEpoch.toString();
+                    databaseReference.child(id).set({
+                      'name': nameController.text.toString(),
+                      "address": placementController.text.toString(),
+                      'id': id
+                    });
+                    Navigator.pop(context);
+                  },
+                );
+              });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  // dialog box
+  Dialog myDialogBo({
+    required BuildContext context,
+    required String name,
+    required String placement,
+    required VoidCallback onPressed,
+  }) {
+    return Dialog(
+      backgroundColor: Colors.yellow,
+      child: Container(
+        decoration: const BoxDecoration(color: Colors.red),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(color: Colors.green),
+                ),
+                CircleAvatar(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.close),
+                  ),
+                ),
+              ],
+            ),
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: "Enter the name"),
+            ),
+            TextField(
+              controller: placementController,
+              decoration:
+                  const InputDecoration(labelText: "Enter the placement"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: onPressed,
+              child: Text(placement),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
